23f5258ee035681f77d9bafe9afdd3301f6a8ac3
Sreeraj <sreerajvijayan2@gmail.com>
2021-06-13 16:46:03 +0530

    Removed unwanted folders

--- RupeekChallenge/app/src/main/java/com/example/rupeekchallenge/Place.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rupeekchallenge
-
-data class Place(
-    val name: String?,
-    val imageUrl: String?,
-    val address: String?,
-    val longitude:String?,
-    val latitude:String?
-)
